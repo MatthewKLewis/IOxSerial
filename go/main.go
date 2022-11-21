821250dbb9bd945c24d5af0ca5b6e602e971a401
MatthewKLewis <matthew.knight.lewis@gmail.com>
2022-11-21 15:35:16 -0500

    repeating api call

--- go/main.go
+++ go/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	//url_alert := "http://52.45.17.177:802/XpertRestApi/api/alert_data"
+	url_location := "http://52.45.17.177:802/XpertRestApi/api/location_data"
+
+	var jsonData = []byte(`{
+		"deviceimei": 111112222233333,
+		"altitude": 1,
+		"latitude": 38.443976,
+		"longitude": -78.874720,
+		"devicetime": 10,
+		"speed": 0,
+		"Batterylevel": "85",
+		"casefile_id": "string",
+		"address": "string",
+		"positioningmode": "string",
+		"tz": "string",
+		"alert_type": "string",
+		"alert_message": "string",
+		"alert_id": "string",
+		"offender_name": "string",
+		"offender_id": "string"
+	}`)
+
+	for true {
+		time.Sleep(time.Second * 3)
+
+		resp, err := http.Post(url_location, "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(resp.Status)
+	}
+
 }
