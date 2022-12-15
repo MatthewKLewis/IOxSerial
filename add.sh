61863551f0056c48c6e651d06cc6ce4e58cccb91
MatthewKLewis <matthew.knight.lewis@gmail.com>
2022-12-15 13:31:44 -0500

    deleted extra add

--- add.sh
+++
@@ -1,7 +0,0 @@
-echo "Adding New..." 
-&& ioxclient docker package postposter ./conf 
-&& cd conf
-&& ioxclient application run portposter package.tar --payload activation.json
-&& cd ..
-&& echo "Listing Active..." 
-&& ioxclient application list
