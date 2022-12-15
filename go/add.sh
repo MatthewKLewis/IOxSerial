09c58c61102e8200a84140e5ca661ddb9151ca24
MatthewKLewis <matthew.knight.lewis@gmail.com>
2022-12-15 13:34:41 -0500

    rename docker image

--- go/add.sh
+++ go/add.sh
@@ -1,1 +1,1 @@
-echo "Adding New..." && ioxclient docker package postposter ./conf && cd conf && ioxclient application run portposter package.tar --payload activation.json && cd .. && echo "Listing Active..." && ioxclient application list
+echo "Adding New..." && ioxclient docker package ioxposter ./conf && cd conf && ioxclient application run portposter package.tar --payload activation.json && cd .. && echo "Listing Active..." && ioxclient application list
